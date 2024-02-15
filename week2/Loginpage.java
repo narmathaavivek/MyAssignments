2490a7d98836bbfc846f5dfc67a8b8bea5a1c8d3
narmathaavivek <159456985+narmathaavivek@users.noreply.github.com>
2024-02-15 12:11:36 +0530

    week2

---
+++ week2/Loginpage.java
@@ -0,0 +1,19 @@
+package seleniumsample;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Loginpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		
+		
+		
+	}
+
+}
